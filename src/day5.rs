c6979031b9c36361aff4751a5f9548789a0b2464
madclaws <pavanan.anandu63@gmail.com>
2022-12-07 13:07:06 +0530

    feat(day5.rs): added stack grid

--- src/day5.rs
+++ src/day5.rs
@@ -5,43 +5,46 @@ pub fn run() {
     let input = tools::load_file("/home/madclaws/labs/aoc_2022/data/day5_test.txt");
     println!("{}", input);
 
-    // create vectors for each stacks
-    // [D]
-    // [N] [C]
-    // [Z] [M] [P]
-
     let stack_and_instructions = input.split("\n\n").collect::<Vec<&str>>();
-
-    println!("{:?}\n", stack_and_instructions);
     
     let stacks_string = stack_and_instructions[0];
     let instructions = stack_and_instructions[1];
 
-    let stack_list = stacks_string.split('\n').collect::<Vec<&str>>();
+    let mut stack_list = stacks_string.split('\n').collect::<Vec<&str>>();
 
     let stack_count = stack_list.last().unwrap().split("  ").count();
-    println!("{:?}\n", stack_count);
 
     let mut stacks: Vec<Vec<&str>> = Vec::new();
 
-    println!("{:?}\n", stack_list);
-    
+    stack_list.pop();
     stack_list.iter()
         .fold(0, |acc: i32, stack| {
-            let str_stack = stack.split(" ").collect::<Vec<&str>>();
-            // str_stack.join(" ");
+            let str_stack = stack.split(' ').collect::<Vec<&str>>();
             stacks.push(str_stack);
             acc
         });
-        
     
-    println!("{:?}\n", stacks);
 
-    // split by \n
-    // skip the first one
-    // [N] [C]
-    // split by space
-    // if ch != "" , then split by '['
-    // add to respective stack
-    // Create a grid out it stack_count *  stack_count?
+    // refactor_stack_grid(&stacks);
+    let mut new_stack: Vec<Vec<&str>> = Vec::new();
+    stacks.iter()
+        .fold(0, |mut acc: i32, row| {
+            new_stack.push(Vec::new());
+            row.iter().fold(0, |mut accu, elem| {
+                if elem.is_empty() {
+                    accu += 1;
+                    if accu == 3 {
+                        new_stack[acc as usize].push("");
+                        accu = 0;
+                    } 
+                } else {
+                    let elements = elem.split("").collect::<Vec<&str>>();
+                    new_stack[acc as usize].push(elements[2])
+                }
+                accu
+            });
+            acc += 1;
+            acc
+        });
+    println!("{:?}", new_stack);
 }
