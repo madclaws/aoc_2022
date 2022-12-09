dfe4dc6af09bcb417129c2f3f657e0f0d0521a53
madclaws <pavanan.anandu63@gmail.com>
2022-12-09 06:08:35 +0530

    feat(day6.rs): day6, init

---
+++ src/day6.rs
@@ -0,0 +1,8 @@
+use crate::tools;
+pub fn run() {
+    println!("\n--- Day 6: Tuning Trouble --- \n");
+    
+    let input = tools::load_file("/home/madclaws/labs/aoc_2022/data/day6_test.txt");
+
+    println!("{}", input);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 mod tools;
 use std::env;
 fn main() {
@@ -15,6 +16,7 @@ fn main() {
         3 => day3::run(),
         4 => day4::run(),
         5 => day5::run(),
+        6 => day6::run(),
         _ => panic!("Invalid day"),
     }
 }
